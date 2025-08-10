beba2134766cbac9bcc291e946da06fb58a81d0d
xiaotianzi <aisuo@live.cn>
2025-08-10 13:20:40 +0800

    This commit introduces an in-browser code bundler using esbuild-wasm. It sets up the UI with a textarea and a button, and wires up the logic to transpile and bundle user-provided JavaScript, displaying the result on the page.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,52 @@
+import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom/client";
+import { useState, useEffect, useRef } from "react";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
 
 const App = () => {
-  return <h1>hi</h1>;
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    })
+  }
+
+  useEffect(() => {
+    startService()
+  }, [])
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()]
+    })
+
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
+  }
+
+  return <div>
+    <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+    <div>
+      <button onClick={onClick}>Submit</button>
+    </div>
+    <pre>{code}</pre>
+  </div>
 };
 
 root.render(<App />);
