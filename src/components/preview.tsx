c6ced04719e0e7f1ee1bddf11777e2574b1fd7cf
xiaotianzi <aisuo@live.cn>
2025-08-16 23:00:26 +0800

    Techniques for Error Handling in React Apps

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import { useRef, useEffect } from "react";
 
 interface PreviewProps {
   code: string;
+  err: string;
 }
 
 const html = `
@@ -11,13 +12,20 @@ const html = `
       <body>
         <div id="root"></div>
         <script>
+        const handleError=(err)=>{
+          const root=document.querySelector('#root');
+          root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>' + err + '</div>';
+          console.error(err);
+        }
+          window.addEventListener('error', (event)=>{
+            event.preventDefault();
+            handleError(event.error);
+          })
           window.addEventListener('message', (event) => {
             try{
               eval(event.data);
             } catch(err){
-              const root=document.querySelector('#root');
-              root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
+              handleError(err);
             }
           },false)
         </script>
@@ -25,7 +33,7 @@ const html = `
     </html>
   `
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframe = useRef<any>();
   useEffect(() => {
     const iframeCurrent = iframe.current;
@@ -47,6 +55,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
         sandbox="allow-scripts"
         srcDoc={html}
       />
+      {err && <div className="preview-error">{err}</div>}
     </div>)
 }
 
