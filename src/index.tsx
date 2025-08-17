e58d8aa9e1d0d81ac71f764a90df2a21505f0bc6
xiaotianzi <aisuo@live.cn>
2025-08-17 14:38:37 +0800

    Creating a Markdown Editor in a React App

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from "react-dom/client";
-import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const el = document.getElementById("root");
 
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   )
 };
