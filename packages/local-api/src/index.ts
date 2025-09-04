285ac9b0dd45ef1d92d07b0f7820f1e1c187238b
xiaotianzi <aisuo@live.cn>
2025-09-04 21:16:21 +0800

    Data Persistence

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -6,6 +6,8 @@ import { createCellsRouter } from "./routes/cells"
 export const serve = (port: number, filename: string, dir: string, useProxy: boolean) => {
     const app = express()
 
+    app.use(createCellsRouter(filename, dir))
+
     if (useProxy) {
         app.use(createProxyMiddleware({
             target: 'http://127.0.0.1:3000',
@@ -16,8 +18,6 @@ export const serve = (port: number, filename: string, dir: string, useProxy: boo
         const packagePath = require.resolve('local-client/build/index.html')
         app.use(express.static(path.dirname(packagePath)))
     }
-
-    app.use(createCellsRouter(filename, dir))
 
     return new Promise<void>((resolve, reject) => {
         const server = app.listen(port)
